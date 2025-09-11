b5f29ee2dc815e5ea30c0e9a6b347f0600b7a3da
DAVID BOAZ <DAVIDBO@il.ibm.com>
2025-09-11 12:55:32 +0300

    toolguard example

--- toolguard/demo_resps.py
+++ toolguard/demo_resps.py
@@ -3,27 +3,27 @@ import asyncio
 from llama_stack.providers.utils.tools.mcp import list_mcp_tools
 from llama_stack_client import LlamaStackClient
 
-from .book_reserv import book_resrervation_tool 
-from .list_airports import list_airports_tool
-
 LLAMA_STACK_URL = "http://localhost:8321/"
-AIRLINE_MCP_URL = "http://localhost:8765/mcp/"
+MCP_URL = "http://localhost:8765/mcp/"
 LLAMA_STACK_MODEL_ID = "openai/gpt-4o"
 
 client = LlamaStackClient(base_url=LLAMA_STACK_URL)
 async def main():
-    tools_resp = await list_mcp_tools(AIRLINE_MCP_URL, {})
+    tools_resp = await list_mcp_tools(MCP_URL, {})
     print(tools_resp.model_dump_json(indent=2))
 
+    # from .book_reserv import book_resrervation_tool 
+    # from .list_airports import list_airports_tool
     response = client.responses.create(
         model=LLAMA_STACK_MODEL_ID,
-        input="get me a list of all available airports.",
-        before_tools_shield= ["my_guard"],
+        # input="list me all doctors (dont apply any filter) and sort them by name.",
+        input="list me all appointements of patient with ssn=12345.",
+        # before_tools_shield= ["my_guard"],
         tools=[
             {
                 "type": "mcp",
-                "server_url": AIRLINE_MCP_URL,
-                "server_label": "Taubench2 Airline MCP",
+                "server_url": MCP_URL,
+                "server_label": "Clinic tools",
             }
             # {
             #     "type": "function",
